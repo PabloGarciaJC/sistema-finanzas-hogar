ada24e9df81cb8440c43af15cf565462833f5ed2
pablogarciajc <pablogarciiajc@gmail.com>
2024-08-02 14:57:13 +0200

    depurar conexion con db

--- config/db.php
+++ config/db.php
@@ -5,15 +5,21 @@ class Database {
         // Conexión a la base de datos usando Docker Compose
         $db = new mysqli('mysql', DB_USER, DB_PASSWORD, DB_DATABASE);   
         
-        // Verificar conexión
-        if ($db->connect_error) {
-            die("Error de conexión: " . $db->connect_error);
+         // Verificar conexión
+         if ($db->connect_error) {
+            error_log("Error de conexión: " . $db->connect_error);
+            die("Error de conexión. Verifica el log para más detalles.");
+        } else {
+            error_log("Conexión exitosa a la base de datos.");
         }
-        
+
         $db->set_charset("utf8");
 
         return $db;
     }
 }
+
+// Prueba la conexión
+Database::connect();
 
 
