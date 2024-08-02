f3c86977518228488a95678221ba72aedc29e08a
pablogarciajc <pablogarciiajc@gmail.com>
2024-08-02 15:05:43 +0200

    update depuracion

--- config/db.php
+++ config/db.php
@@ -2,6 +2,12 @@
 class Database {
 
     static public function connect(){
+
+         // Verificar y registrar variables de entorno
+         error_log("DB_USER: " . (defined('DB_USER') ? DB_USER : 'No definido'));
+         error_log("DB_PASSWORD: " . (defined('DB_PASSWORD') ? 'Definido' : 'No definido'));
+         error_log("DB_DATABASE: " . (defined('DB_DATABASE') ? DB_DATABASE : 'No definido'));
+         
         // Conexión a la base de datos usando Docker Compose
         $db = new mysqli('mysql', DB_USER, DB_PASSWORD, DB_DATABASE);   
         
