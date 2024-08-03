14dc0ba231cacda23297a5a208f7c99bffc8c680
pablogarciajc <pablogarciiajc@gmail.com>
2024-08-03 21:33:22 +0200

    se elimina logs

--- config/db.php
+++ config/db.php
@@ -1,29 +1,19 @@
 <?php
-// Mostrar todos los errores para depuración
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 class Database {
     static public function connect() {
+        $db = new mysqli('127.0.0.1', 'u498565300_fzuserpjgc', 'iTdJM0k4w6c4qS', 'u498565300_fhbdpjgc', 3306);
 
-        // Crear conexión
-        $db = new mysqli(DB_SERVER_NAME, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT);
-
-        // Verificar conexión
         if ($db->connect_error) {
             die("Error de conexión: " . $db->connect_error);
         } else {
-            // echo "Conexión exitosa a la base de datos.";
+            echo "Conexión exitosa a la base de datos.";
         }
 
-        // Establecer el conjunto de caracteres
         $db->set_charset("utf8");
 
         return $db;
     }
 }
 
-// Probar la conexión
 Database::connect();
 ?>
