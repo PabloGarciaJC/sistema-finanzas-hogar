6699b42ea3fe97ecc0e99c2f381337791e9086dc
pablogarciajc <pablogarciiajc@gmail.com>
2024-08-03 18:12:53 +0200

    se elimina logs

--- config/db.php
+++ config/db.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 class Database {
     static public function connect() {
         $db = new mysqli('127.0.0.1', 'u498565300_fzuserpjgc', 'iTdJM0k4w6c4qS', 'u498565300_fhbdpjgc', 3306);
