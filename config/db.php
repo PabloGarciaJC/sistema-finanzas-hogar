8624df39645d70a47136cc6bfd640cd2b7efd8e4
pablogarciajc <pablogarciiajc@gmail.com>
2024-08-03 17:51:40 +0200

    se elimina logs

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 class Database {
 
     static public function connect(){
-        // $db = new mysqli('127.0.0.1', 'u498565300_fzuserpjgc', 'iTdJM0k4w6c4qS', 'u498565300_fhbdpjgc', 3306);
+        $db = new mysqli('127.0.0.1', 'u498565300_fzuserpjgc', 'iTdJM0k4w6c4qS', 'u498565300_fhbdpjgc', 3306);
         echo "Conexión exitosa a la base de datos.";
         // if ($db->connect_error) {
         //     echo "Error de conexión: " . $db->connect_error;
