253ec68b1066f82df927972bd3e5390d134bb96e
pablogarciajc <pablogarciiajc@gmail.com>
2024-08-03 16:02:29 +0200

    se implementa logger para las clases

--- config/db.php
+++ config/db.php
@@ -1,22 +1,22 @@
 <?php
 class Database {
 
+    private static $logger;
+
     static public function connect(){
 
-         // Verificar y registrar variables de entorno
-         error_log("DB_USER: " . (defined('DB_USER') ? DB_USER : 'No definido'));
-         error_log("DB_PASSWORD: " . (defined('DB_PASSWORD') ? 'Definido' : 'No definido'));
-         error_log("DB_DATABASE: " . (defined('DB_DATABASE') ? DB_DATABASE : 'No definido'));
-         
+        if (self::$logger === null) {
+            self::$logger = LoggerWrapper::getInstance();
+        }
+
         // Conexión a la base de datos usando Docker Compose
         $db = new mysqli('mysql', DB_USER, DB_PASSWORD, DB_DATABASE);   
         
-         // Verificar conexión
-         if ($db->connect_error) {
-            error_log("Error de conexión: " . $db->connect_error);
-            die("Error de conexión. Verifica el log para más detalles.");
+        // Verificar conexión
+        if ($db->connect_error) {
+            self::$logger->info("Error de conexión: " . $db->connect_error);
         } else {
-            error_log("Conexión exitosa a la base de datos.");
+            self::$logger->info("Conexión exitosa a la base de datos.");
         }
 
         $db->set_charset("utf8");
@@ -24,8 +24,3 @@ class Database {
         return $db;
     }
 }
-
-// Prueba la conexión
-Database::connect();
-
-
