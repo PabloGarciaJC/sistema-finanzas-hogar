658745790bdf2745ca3af61690d9565d9166e61a
pablogarciajc <pablogarciiajc@gmail.com>
2024-08-03 21:56:18 +0200

    se elimina logs

--- config/db.php
+++ config/db.php
@@ -5,7 +5,23 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 class Database {
+
+    private static $logger;
+
+    public function __construct()
+    {
+        // No es necesario en este caso, ya que estamos usando el método estático
+    }
+
+    static public function initLogger() {
+        self::$logger = LoggerWrapper::getInstance();
+    }
+
     static public function connect() {
+        // Asegúrate de haber inicializado el logger
+        if (self::$logger === null) {
+            self::initLogger();
+        }
 
         // Crear conexión
         $db = new mysqli(DB_SERVER_NAME, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT);
@@ -14,8 +30,8 @@ class Database {
         if ($db->connect_error) {
             die("Error de conexión: " . $db->connect_error);
         } else {
-            // echo "Conexión exitosa a la base de datos.";
-            LoggerWrapper::getInstance()->info('Conexión exitosa a la base de datos.');
+            // Usar el logger estático
+            self::$logger->info('Conexión exitosa a la base de datos.');
         }
 
         // Establecer el conjunto de caracteres
@@ -24,6 +40,9 @@ class Database {
         return $db;
     }
 }
+
+// Inicializar el logger
+Database::initLogger();
 
 // Probar la conexión
 Database::connect();
