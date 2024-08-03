cc70ad5a6934589091b37a9142ac474a293dd2fd
pablogarciajc <pablogarciiajc@gmail.com>
2024-08-03 18:05:31 +0200

    se elimina logs

--- config/db.php
+++ config/db.php
@@ -1,41 +1,20 @@
 <?php
-class Database {
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-    static public function connect(){
-        echo "Intentando conectar a la base de datos...<br>";
+$host = '127.0.0.1';
+$username = 'u498565300_fzuserpjgc';
+$password = 'iTdJM0k4w6c4qS';
+$dbname = 'u498565300_fhbdpjgc';
 
-        // Mostrar errores de PHP
-        error_reporting(E_ALL);
-        ini_set('display_errors', 1);
+$conn = new mysqli($host, $username, $password, $dbname);
 
-        // Intentar la conexión a la base de datos
-        $db = new mysqli('127.0.0.1', 'u498565300_fzuserpjgc', 'iTdJM0k4w6c4qS', 'u498565300_fhbdpjgc', 3306);
-
-        // Verificar conexión
-        if ($db->connect_error) {
-            echo "Error de conexión: " . $db->connect_error . "<br>";
-            return null; // Salir si hay error de conexión
-        }
-
-        echo "Conexión exitosa a la base de datos.<br>";
-
-        // Verificar si la conexión está activa
-        if ($db->ping()) {
-            echo "La conexión a la base de datos está activa.<br>";
-        } else {
-            echo "La conexión a la base de datos no está activa.<br>";
-        }
-
-        // Configurar charset
-        if ($db->set_charset("utf8")) {
-            echo "Charset configurado a utf8.<br>";
-        } else {
-            echo "Error configurando charset: " . $db->error . "<br>";
-        }
-
-        return $db;
-    }
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
 }
 
-// Probar la conexión
-Database::connect();
+echo "Conexión exitosa a la base de datos.";
+
+$conn->close();
+?>
