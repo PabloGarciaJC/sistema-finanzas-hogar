2fe37cd0dc17322511cd1c62e85772e69357b2ef
pablogarciajc <pablogarciiajc@gmail.com>
2024-08-04 03:39:53 +0200

    se elimina logs

--- views/layout/header.php
+++ views/layout/header.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-  <title>Finanzas para el hogar</title>
+  <title>Finanzas Hogar</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="keywords" content="Modern Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
