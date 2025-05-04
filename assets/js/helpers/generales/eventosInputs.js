0e17bc9726a9099b2eb79f5af243fa4d7807bd69
PabloGarciaJC <pablogarciajc@hotmail.com>
2025-05-05 00:43:49 +0200

    se eliminan ficheros innecesarios

--- assets/js/helpers/generales/eventosInputs.js
+++
@@ -1,6 +0,0 @@
-function eventoInputs(elemento, evento, idElemento) {
-  elemento.addEventListener(evento, (e) => {
-    let id = document.querySelector(`#${idElemento}`);
-    id.lastElementChild.innerHTML = "";
-  });
-}
--- controllers/ErrorController.php
+++
@@ -1,7 +0,0 @@
-<?php
-    class ErrorController{
-        public function index(){
-            echo "<h1> Error 404, La Pagina que Buscas No Existe </h1>";
-        }
-    }
-?>
