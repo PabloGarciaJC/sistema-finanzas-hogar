75c104cbf46be502791dc57f50f40abe560ffe84
PabloGarciaJC <pablogarciajc@hotmail.com>
2025-05-24 20:00:18 +0200

    se implementa, autenticacion base con su paquete

---
+++ migrations/Version20250524172539.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250524172539 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create table user for authentication';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE user (
+            id INT AUTO_INCREMENT NOT NULL,
+            email VARCHAR(180) NOT NULL UNIQUE,
+            roles JSON NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            PRIMARY KEY(id)
+        ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE user');
+    }
+}
